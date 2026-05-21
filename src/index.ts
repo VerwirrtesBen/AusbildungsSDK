7f3f50f436f4c68db76b8aa9effd36b29f01ebb0
VerwirrtesBen <Benr.kontakt+git@gmail.com>
2026-05-21 15:28:37 +0200

    hinzufüggen einer readme datei

--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ async function  printSite(seite:number):Promise<void>{
         sfe:"93574;93575;93581;93583;93584;93592;93593;93598;93611;93621;93625;93627;93638;93648;93649;93650;93651;93659;93661;93677;93685;93690;93694;93696;93698;93699;93701;93705;93713;93718;93719;93720;93724;93733;93736;93739;93751;93757;93767;93772;93774;93795;93796;93797;93799;93802;93804;93813"
     });
     console.log(angebotResponse.getPage());
+    console.log(A.getStudienfelder())
 
     for(let i = 0; i<1; i++){
         const sA = angebotResponse.getAngebotliste()[i];
