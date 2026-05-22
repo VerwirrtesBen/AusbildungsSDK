5efccbfeb7eddf6ae6c9c43b6314f1736f8e586a
VerwirrtesBen <Benr.kontakt+git@gmail.com>
2026-05-22 09:32:33 +0200

    vereinfachung der index.ts als Anwendungsbeispiel

--- src/index.ts
+++ src/index.ts
@@ -2,77 +2,15 @@ import {AngeboteProvider} from "./AngeboteProvider.js"
 import {azubiAngebotResponse } from "./azubiAngebotResponse.js";
 import {studiAngebotResponse} from "./studiAngebotResponse.js"
 
+// Dieser code Dient als Beispiel zur verwendung der SDK
 
-var A:AngeboteProvider = new AngeboteProvider;
+const Provider = new AngeboteProvider();
 
-async function  printSite(seite:number):Promise<void>{
-    const angebotResponse: studiAngebotResponse = await A.getStudiAngebote({
-        pg:seite,
-        sfe:"93574;93575;93581;93583;93584;93592;93593;93598;93611;93621;93625;93627;93638;93648;93649;93650;93651;93659;93661;93677;93685;93690;93694;93696;93698;93699;93701;93705;93713;93718;93719;93720;93724;93733;93736;93739;93751;93757;93767;93772;93774;93795;93796;93797;93799;93802;93804;93813"
-    });
-    console.log(angebotResponse.getPage());
-    console.log(A.getStudienfelder());
+// Ausbildung
+const AzubiAngebotListe:azubiAngebotResponse = await Provider.getAzubiAngebote({bart:102}); // bart 102 --> nur Berufsausbildungen
+console.log(AzubiAngebotListe.getAngebotliste()[1].getAbschlussbezeichnung());
 
-    for(let i = 0; i<19; i++){
-        const sA = angebotResponse.getAngebotliste()[i];
-        console.log(sA.getHochschulart());
-        console.log(sA.getLaengengrad());
-        console.log(sA.getBreitengrad())
-        console.log(sA.getLogo());
-        console.log(sA.getName());
-        console.log(sA.getOrt());
-        console.log(sA.getPlz());
-        console.log(sA.getRegion());
-        console.log(sA.getStrasse());
-        console.log(sA.getStudienbeginn());
-        console.log(sA.getStudienfach());
-        console.log(sA.getStudienform());
-        console.log(sA.getStudientyp());
+// Studium
+const StudiAngebotListe:studiAngebotResponse = await Provider.getStudiAngebote({sw:"Informatikberufe"}); // sw --> Beispiel Suchwort
+console.log(StudiAngebotListe.getAngebotliste()[2].getStudienfach());
 
-        /*
-        console.log(A.getAbschlussart());
-        console.log(A.getAbschlussbezeichnung());
-        console.log(A.getAuchGeeignetFuer());
-        console.log(A.getAusbildungBreitengrad());
-        console.log(A.getAusbildungLaengengrad());
-        console.log(A.getAusbildungStrasse());
-        console.log(A.getAusbildungsOrt());
-        console.log(A.getAusbildungsinhalte());
-        console.log(A.getAusbilungPlz());
-        console.log(A.getBeginn());
-        console.log(A.getBildunganbieterBezeichnung());
-        console.log(A.getBildungsart());
-        console.log(A.getDienstleistungen());
-        console.log(A.getE_mail());
-        console.log(A.getEnde());
-        console.log(A.getFörderung());
-        console.log(A.getHinweiseZurAdresse());
-        console.log(A.getId());
-        console.log(A.getIndividuellerEinstieg());
-        console.log(A.getInternet());
-        console.log(A.getLink());
-        console.log(A.getLogo());
-        console.log(A.getMobilTelefon());
-        console.log(A.getOrt());
-        console.log(A.getPlz());
-        console.log(A.getPrüfendeStelle());
-        console.log(A.getSchulart());
-        console.log(A.getStraße());
-        console.log(A.getTeilnehmerzahl());
-        console.log(A.getTelefon());
-        console.log(A.getUnterrichtsform());
-        console.log(A.getZielgruppe());
-        console.log(A.getZugang());
-        console.log(A.getZulassungZurFörderungPerBildungsgutschein());
-        console.log(A.getZusatzqualifizierung());
-        */
-    }
-    
-}
-
-
-for(let i = 0; i <227; i++){
-     console.log(`Seite ${i} wird angefragt:`);
-    await new Promise(f => setTimeout(f, 500));
-    await printSite(i);
-}
